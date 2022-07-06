dfd7c4e109270d6d02d07d0f4edaf6cb3ffe905a
Sukhendu Sekhar Guria <76804228+Sukhendu2002@users.noreply.github.com>
2022-07-06 17:01:42 +0530

    del client

--- client/src/routing/PrivateRoute.js
+++
@@ -1,7 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-
-const PrivateRoute = () => {
-  let auth = localStorage.getItem("authToken");
-  return auth ? <Outlet /> : <Navigate to="/login" />;
-};
-export default PrivateRoute;
